79dbb2962708c649165b9546d013dace3849098d
NikkyXO <alexisdame2017@gmail.com>
2022-08-24 19:06:15 +0100

    Binary

--- binary_trees.h
+++ binary_trees.h
@@ -17,10 +17,10 @@
  */
 typedef struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
 } binary_tree_t;
 
 typedef struct binary_tree_s bst_t;
@@ -31,12 +31,12 @@ void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
-binary_tree_is_root(const binary_tree_t *node);
+int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
-binary_tree_is_full(const binary_tree_t *tree);
-binary_tree_t *binary_tree_sibling;
+int binary_tree_is_full(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
 static size_t _height(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
