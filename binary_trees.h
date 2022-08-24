99eaab61d2b2f850c7df94e2d362244526b492c4
Jasmine <jasmineolofin@gmail.com>
2022-08-24 00:31:03 -0700

    Added binary_tree_postorder prototype to header file

--- binary_trees.h
+++ binary_trees.h
@@ -29,6 +29,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *, int);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *, int);
 int binary_tree_is_leaf(const binary_tree_t *);
 void binary_tree_preorder(const binary_tree_t *, void (*)(int));
+void binary_tree_postorder(const binary_tree_t *, void (*)(int));
 
 
 #endif
