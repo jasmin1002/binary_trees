4a69d141a5abaf4af1e6db146dd5570808d2c8f5
Jasmine <jasmineolofin@gmail.com>
2022-08-23 17:23:41 -0700

    Made adjustment to betty compliance

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,17 +9,13 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node = malloc(sizeof(binary_tree_t));
-
 	if (node == NULL)
-		return NULL;
-	else if (parent == NULL)
-		return NULL;
-	else
-		node = binary_tree_node(parent, value);
+		return (NULL);
+	if (parent == NULL)
+		return (NULL);
 
 	if (parent->right == NULL)
-		parent->right = node;
+		parent->right = binary_tree_node(parent, value);
 	else
 	{
 		node->right = parent->right;
